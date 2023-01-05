cc142496641bb4af7d7e252460eaed56c6257cff
Akaarit-Frontend <mehedi.emon@akaarit.com>
2023-01-05 16:03:58 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,83 +6,29 @@
         new WOW().init();
     });
 
-    // Swipe Slider
+    // Offer Slider
 
-    // $(document).ready(function () {
-    //     var swiper = new Swiper(".hero-banner-wpr", {
-    //       autoplay: {
-    //         delay: 5000,
-    //         disableOnInteraction: false
-    //       },
-    //       speed: 500,
-    //       loop: true,
-    //       pagination: {
-    //         el: ".swiper-pagination",
-    //         type: "fraction"
-    //       },
-    //       navigation: {
-    //         nextEl: ".swiper-button-next",
-    //         prevEl: ".swiper-button-prev"
-    //       },
-    //       on: {
-    //         init: function () {
-    //           $(".swiper-progress-bar").removeClass("animate");
-    //           $(".swiper-progress-bar").removeClass("active");
-    //           $(".swiper-progress-bar").eq(0).addClass("animate");
-    //           $(".swiper-progress-bar").eq(0).addClass("active");
-    //         },
-    //         slideChangeTransitionStart: function () {
-    //           $(".swiper-progress-bar").removeClass("animate");
-    //           $(".swiper-progress-bar").removeClass("active");
-    //           $(".swiper-progress-bar").eq(0).addClass("active");
-    //         },
-    //         slideChangeTransitionEnd: function () {
-    //           $(".swiper-progress-bar").eq(0).addClass("animate");
-    //         }
-    //       }
-    //     });
-    //     $(".swiper-container").hover(function () {
-    //       swiper.autoplay.stop();
-    //       $(".swiper-progress-bar").removeClass("animate");
-    //     }, function () {
-    //       swiper.autoplay.start();
-    //       $(".swiper-progress-bar").addClass("animate");
-    //     });
-    // });
-
-    var swiper = new Swiper(".trv-hero-banner", {
-        // navigation: {
-        //   nextEl: ".swiper-button-next",
-        //   prevEl: ".swiper-button-prev",
-        // },
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        speed: 500,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            type: "fraction"
-        },
-        on: {
-            init: function () {
-              $(".swiper-progress-bar").removeClass("animate");
-              $(".swiper-progress-bar").removeClass("active");
-              $(".swiper-progress-bar").eq(0).addClass("animate");
-              $(".swiper-progress-bar").eq(0).addClass("active");
-            },
-            slideChangeTransitionStart: function () {
-              $(".swiper-progress-bar").removeClass("animate");
-              $(".swiper-progress-bar").removeClass("active");
-              $(".swiper-progress-bar").eq(0).addClass("active");
-            },
-            slideChangeTransitionEnd: function () {
-              $(".swiper-progress-bar").eq(0).addClass("animate");
-            }
+    
+    $(".offers-slider").owlCarousel({
+        items:2.5,
+        loop:true,
+        autoplay:false,
+        slideBy: 2,
+        nav:true,
+        navText: ["<i class='icofont-thin-left'></i>","<i class='icofont-thin-right'></i>"],
+        dots:false,
+        responsive:{
+            // 0:{
+            //     items: 2,
+            //     center:false
+            // },
+            // 575:{
+            //     items: 5,
+            //     center:true,
+            // },
         }
-    });
-
+    })
+    
     // Services Slider
 
     $(".services-wpr").owlCarousel({
